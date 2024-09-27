25893fd0d7143c94d894c87b6ae8527b4e8aea1a
riya131513gupta <riyagupta131513@gmail.com>
2024-09-27 12:04:27 +0530

    Add files via upload

---
+++ sizeof.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int i;
+	float f;
+	double d;
+	printf("the size of integer is : %lu\n" , sizeof(i));
+	printf("the size of floating is : %lu\n" , sizeof(f));
+	printf("the size of double is : %lu\n " , sizeof(d));
+	return 0;
+}
