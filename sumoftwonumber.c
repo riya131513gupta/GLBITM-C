4f1320168d59b15c71e674b512345b3a22b50046
riya131513gupta <riyagupta131513@gmail.com>
2024-09-27 12:18:09 +0530

    Add files via upload

---
+++ sumoftwonumber.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int a, b, sum;
+	printf("enter the value of a,b");
+	scanf("%d %d", &a,&b);
+	sum = a+b;
+	printf("the sum of %d , %d is :", sum);
+	return 0;
+}
