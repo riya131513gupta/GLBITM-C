1c1c5331a43027303ce2a89623f0a34005077660
riya131513gupta <riyagupta131513@gmail.com>
2024-09-27 11:50:08 +0530

    Add files via upload

---
+++ evenodd.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+  int a;
+  printf("enter the value of a :");
+  scanf("%d", &a);
+  if(a%2==0)
+  {
+    printf("the num is even ...");
+  }   
+
+  else
+  {
+    printf("thr number is not even..");
+  }
+  return 0;
+}
